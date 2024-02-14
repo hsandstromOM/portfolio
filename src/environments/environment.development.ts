c505c09457b5e193c297d9c18a9969a6708f51b5
hoseasandstrom <57154575+breadandButterPR@users.noreply.github.com>
2024-02-14 09:56:23 -0500

    temp commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+  contentful: {
+    space: 'v5nruat6w4lq',
+    accessToken: 'BThHUTvSR9cFJd2tKqqv80M-8OHHMw0tTQlL5GMx5Ko',
+    apiHost: 'https://cdn.contentful.com',
+    previewToken: 'zduQBW_YXNqnpKPdLaz-VvGSQILDJdx-hYOw0VWf6RU',
+    previewHost: 'preview.contentful.com'
+  },
+    hostUrl: "http://localhost:4000",
+    urlToBackend: "https://hosea-portfolio.firebaseapp.com/",
+    mapKey: "XXXXXX-XXXXXX"
+};
